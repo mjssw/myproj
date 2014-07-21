d6f4f72ece01d3c18ab36c70a49793fe00267317
shenjiajia1225 <shenjiajia1225@163.com>
2014-07-21 11:14:45 +0800

    add linux libmysql
    1. add linux libmysql and modify set deps script to set libmysqlclient

--- third_part/set_server_deps.sh
+++ third_part/set_server_deps.sh
@@ -8,3 +8,11 @@ sudo cp ./log4cxx/lib/liblog4cxx.* "$DEPS_PATH"
 sudo cp ./protobuf/lib/libprotobuf* "$DEPS_PATH"
 sudo cp ./tinyxml/lib/libtinyxml* "$DEPS_PATH"
 #sudo echo "$DEPS_PATH" >> /etc/ld.so.conf
+
+MYSQL_LIB_PATH="/usr/lib"
+sudo cp ./mysql/lib/libmysqlclient.so.16.0.0 "$MYSQL_LIB_PATH"
+sudo cp ./mysql/lib/libmysqlclient_r.so.16.0.0 "$MYSQL_LIB_PATH"
+sudo ln -s $MYSQL_LIB_PATH/libmysqlclient_r.so.16.0.0 $MYSQL_LIB_PATH/libmysqlclient_r.so.16
+sudo ln -s $MYSQL_LIB_PATH/libmysqlclient.so.16.0.0 $MYSQL_LIB_PATH/libmysqlclient.so.16
+sudo ln -s $MYSQL_LIB_PATH/libmysqlclient.so.16 $MYSQL_LIB_PATH/libmysqlclient.so
+sudo ln -s $MYSQL_LIB_PATH/libmysqlclient.so.16 $MYSQL_LIB_PATH/libmysqlclient
